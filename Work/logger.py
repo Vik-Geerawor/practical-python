2bde84f063f5b273052b4cdff8334e9a2da5c1a7
vik <sgeerawor@gmail.com>
2022-11-27 01:41:58 +0000

    handled creation of logs dir as it's excluded

--- Work/logger.py
+++ Work/logger.py
@@ -5,10 +5,16 @@ which is then used everywhere in the code
 The root loggers logs to the application.log file
 """
 
-
+import os
 import json
-import logging
 from logging.config import dictConfig
+
+# ensures that the logs dir is present in the root dir
+logs_dir = "../logs"
+
+if not (os.path.isdir(logs_dir)):
+    os.mkdir(logs_dir)
+
 
 with open('../config/logger.json') as f:
     logging_config = json.load(f)
